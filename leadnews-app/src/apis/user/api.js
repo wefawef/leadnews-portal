a99afce0c2f07f3a8c764e7f58663c8073bfc730
duanss <2499165406@qq.com>
2026-01-15 19:09:16 +0800

    feat:完善个人信息功能

--- leadnews-app/src/apis/user/api.js
+++ leadnews-app/src/apis/user/api.js
@@ -28,9 +28,8 @@ Api.prototype = {
     upUserImage : function(userId, image){
         let url = 'http://127.0.0.1:51601/user/api/v1/personal/upUserImage';
         const formData = {
-            apUser: JSON.stringify({ id: userId }),
             id: String(userId || ''),
-            Image: image
+            image: image
         };
         return new Promise((resolve, reject) => {
             this.vue.$request.postMultipart(url, formData).then((d)=>{
